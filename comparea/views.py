7cedd9145abff8790dd6db261739b29776b460fc
Dan Vanderkam <danvdk@gmail.com>
2014-07-10 16:24:46 -0400

    serving /A+B URLs

--- comparea/views.py
+++ comparea/views.py
@@ -4,7 +4,17 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    shape1 = models.feature_for_iso3('AUS')
-    shape2 = models.feature_for_iso3('USA')
+    return pair('USA', 'AUS')
+
+@app.route('/<id1>+<id2>')
+def pair(id1, id2):
+    shape1 = models.feature_for_iso3(id1)
+    shape2 = models.feature_for_iso3(id2)
     name_id_pairs = models.all_countries()
+
+    if not shape1:
+        return "No feature with id %s" % id1, 400
+    if not shape2:
+        return "No feature with id %s" % id2, 400
+
     return render_template('index.html', shape1=shape1, shape2=shape2, name_id_pairs=name_id_pairs)
