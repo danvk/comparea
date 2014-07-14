5e6527da5f845947a8d014b6be631bdb266d07a6
Dan Vanderkam <danvdk@gmail.com>
2014-07-14 14:51:52 -0400

    basic resize support

--- comparea/static/js/render.js
+++ comparea/static/js/render.js
@@ -8,4 +8,13 @@ $(function() {
   addChooserListeners();
 
   setDisplayForFeatures(geojson_features);
+
+  $(window).on('resize', function() {
+    var width = document.getElementById('svg-container').offsetWidth,
+        height = document.getElementById('svg-container').offsetHeight;
+    d3.select('#svg-container svg')
+        .attr('width', width)
+        .attr('height', height);
+
+  });
 });
