d60221bbb1c88fa3254959945c10d8ba4ad9f5c1
Dan Vanderkam <danvdk@gmail.com>
2014-07-14 14:24:46 -0700

    inline <option> arrays

--- comparea/static/js/render.js
+++ comparea/static/js/render.js
@@ -1,10 +1,4 @@
 $(function() {
-  var getId = function(i) {
-    return geojson_features[i].id;
-  };
-  populateChoosers(name_id_pairs);
-  $('#choose0 option[value=' + getId(0) + ']').attr('selected', true);
-  $('#choose1 option[value=' + getId(1) + ']').attr('selected', true);
   addChooserListeners();
 
   setDisplayForFeatures(geojson_features);
