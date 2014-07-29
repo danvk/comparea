ea9d7dd4de93a7990d163a170ae7efe5cd268aba
Dan Vanderkam <danvdk@gmail.com>
2014-07-28 23:26:13 -0700

    beginnings of a responsive UI

--- comparea/static/js/render.js
+++ comparea/static/js/render.js
@@ -2,6 +2,11 @@ $(function() {
   $(".choose").select2();
   addChooserListeners();
 
+  // Hide the address bar!
+  setTimeout(function(){
+    window.scrollTo(0, 1);
+  }, 0);
+
   setDisplayForFeatures(geojson_features);
 
   $(window).on('resize', function() {
