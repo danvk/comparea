917b248714478ac3728076c040fe593c041a4363
Dan Vanderkam <danvdk@gmail.com>
2014-08-04 09:32:55 -0400

    add port config param

--- config.py
+++ config.py
@@ -2,9 +2,14 @@ class Default:
     DEBUG=False
     VERSION=1
     USE_THIRD_PARTY_CDN=False
+    PORT = None  # default
 
 class Development:
     DEBUG=True
+
+class Testing:
+    TESTING = True
+    PORT = 5001  # avoid conflicts with a running dev server
 
 class Production:
     USE_THIRD_PARTY_CDN=True
--- run.py
+++ run.py
@@ -3,7 +3,7 @@
 import comparea
 
 def run():
-    comparea.app.run(host='0.0.0.0')
+    comparea.app.run(host='0.0.0.0', port=comparea.app.config['PORT'])
 
 if __name__ == '__main__':
     run()
