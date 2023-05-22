3518cdc200b52d49ae73f382664cc3fec09536f1
Dan Vanderkam <danvdk@gmail.com>
2023-05-22 15:57:21 -0400

    Matching JSON endpoint

--- server/templates.ts
+++ server/templates.ts
@@ -3,10 +3,6 @@ import { sprintf } from 'sprintf-js';
 
 import { CompareaFeature } from './api';
 
-export function renderPanel(f: CompareaFeature): string {
-  return safeHtml`<p>Name: ${f.properties.name}</p>`;
-}
-
 /** Returns a string like '10%' or '123 times'. Assumes ratio>1. */
 export function formatRatio(ratio: number): string {
   if (ratio < 2) {
@@ -42,4 +38,35 @@ export function pageTitle(f1: CompareaFeature, f2: CompareaFeature): string {
   const name1 = f1.properties.name;
   const name2 = f2.properties.name;
   return `${name1} vs. ${name2}: Comparea Area Comparison`;
+}
+
+export function renderPanel(f: CompareaFeature): string {
+  const props = f.properties;
+  return safeHtml`
+<div class="statistics">
+  <p>
+    <span class=stat-name>Area</span>:
+    <span class="stat-value area">${Math.round(
+      props.area_km2
+    ).toLocaleString()} km<sup>2</sup></span>
+    <a class="stat-source" href="${props.area_km2_source_url}">${
+    props.area_km2_source
+  }</a>
+  </p>
+  <p class="population">
+    <span class="stat-name">Population</span>:
+    <span class="stat-value">${Math.round(
+      props.population
+    ).toLocaleString()}</span>
+    (<span class="stat-year">${props.population_date}</span>)
+    <a class="stat-source" href="${props.population_source_url}">${
+    props.population_source
+  }</a>
+   </p>
+</div>
+<p class="description">
+  ${props.description}
+  <a href="${props.wikipedia_url}">Wikipedia</a>
+</p>
+  `;
 }
